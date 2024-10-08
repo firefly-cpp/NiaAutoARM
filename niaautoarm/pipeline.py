2365f66ad562268eb646a0afe1f87090ae04c967
Uroš Mlakar <uros88mlakar@gmail.com>
2024-10-08 10:31:53 +0200

    Small fix to pipeline

--- niaautoarm/pipeline.py
+++ niaautoarm/pipeline.py
@@ -16,12 +16,17 @@ class Pipeline:
         self.parameters = parameters
         self.fitness = fitness
         self.rules = rules
-        self.support = rules.mean("support")            
-        self.confidence = rules.mean("confidence")
+        self.support = 0
+        self.confidence = 0
+
+        if len(rules) > 0:
+            self.support = rules.mean("support")            
+            self.confidence = rules.mean("confidence")
+
         self.num_rules = len(rules)
 
 
     def __str__(self):
-        return "\nPreprocessing: {}\nAlgorithm: {}\nHyperparameters: {}\nMetrics: {}\nFitness: {:.4f}\nMean Support: {:.4f}\nMean Confidence: {:.4f}\n# rules: {}\n------------------".format(
+        return "\nPreprocessing: {}\nAlgorithm: {}\nHyperparameters: {}\nMetrics: {}\nFitness: {:.4f}\nMean Support: {:.4f}\nMean Confidence: {:.4f}\nRules: {}\n------------------".format(
             self.preprocessing, self.algorithm, self.parameters, self.metrics, self.fitness, self.support, self.confidence, self.num_rules)
     
