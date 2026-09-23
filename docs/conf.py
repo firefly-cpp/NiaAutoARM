826373328a2a4b806c4b7fde9124e58f6e097af0
Iztok Fister Jr. <iztok@iztok.dev>
2026-09-23 10:25:08 +0200

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2025-2026, Uroš Mlakar, Iztok Fister Jr.'
 author = 'Uroš Mlakar, Iztok Fister Jr.'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 # -- General configuration ---------------------------------------------------
 
